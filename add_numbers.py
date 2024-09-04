5fb78fd8003be0ff7e91d03e02f5a6f3cb07b678
christopherkiprop <choppachepa@gmail.com>
2024-09-04 14:34:47 +0300

    added logic for car.py

--- add_numbers.py
+++ add_numbers.py
@@ -4,6 +4,6 @@ def add_numbers(num1, num2):
 
 # Example usage
 if __name__ == "__main__":
-    num1 = 5
-    num2 = 7
+    num1 = 100
+    num2 = 100
     print(f"The sum of {num1} and {num2} is {add_numbers(num1, num2)}")
--- car.py
+++ car.py
@@ -1,9 +1,9 @@
-def __init__(car_instance, make, model, year):
+class car:
+ def __init__(car_instance, make, model, year):
      # Initialize car information
     car_instance.make = make
     car_instance.model = model
     car_instance.year = year
 
-def display_info(car_instance):
-    # Print car information
-    print(f"Make: {car_instance.make}, Model: {car_instance.model}, Year: {car_instance.year}")
+car_one=["toyota camry","bmw m3"]
+print(car_one)
